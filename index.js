a4f202aad2e3fe10626cd89a01e6021c99037303
Anastasia Shatokhina <shnatya@gmail.com>
2021-10-31 22:06:41 -0700

    Commented functions

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let arrayOfFacilityID = []
+//when Submit pushed, clear the table and send fetch request 
+//listen for buttons Clear All and Select All
 document.addEventListener('DOMContentLoaded', () => {
     const form = document.querySelector('#activity-form')
     form.addEventListener('submit', (event) => {
@@ -13,10 +14,12 @@ document.addEventListener('DOMContentLoaded', () => {
     const buttonSelectAll =document.querySelector("#Select-All")
     buttonSelectAll.addEventListener('click', () => handleSelectAll())
 })
+//function clears the table
 function clearTable(){
     const tableBody = document.getElementById('table-body')
     tableBody.innerHTML = ' '                       //innerHTML?
 }
+//function sends request to API to get data of Facilities
 function fetchFacilities() {
     fetch("https://ridb.recreation.gov/api/v1/facilities?apikey=", {
         method: 'GET',
@@ -28,10 +31,12 @@ function fetchFacilities() {
         .then((data => proccessData(data.RECDATA))
     )
 }
+//function gets array of checked checkboxes and gives it to sortFacility function
 function proccessData(facilitiesArray){
     const arrayOfCheckedBoxes = readCheckBoxes()
     facilitiesArray.forEach(facilityObj => sortFacility(facilityObj, arrayOfCheckedBoxes))
 }
+//function checkes what checkboxes were checked and return an array of checked checkboxes
 function readCheckBoxes(){
     const NodeListOfCheckedBoxes = document.querySelectorAll('input[type = "checkbox"]:checked')
     let arrayOfCheckedBoxes = []
@@ -40,6 +45,7 @@ function readCheckBoxes(){
     }
      return arrayOfCheckedBoxes
 }
+//according to array of checked checkboxes function chooses what facility should be rendered
 function sortFacility(facilityObj, arrayOfCheckedBoxes) {
     
     arrayOfCheckedBoxes.forEach(checkBox => {
@@ -48,6 +54,8 @@ function sortFacility(facilityObj, arrayOfCheckedBoxes) {
                                          }
                                )    
 }
+//function renders facility by creating a new table row with next table columns: Name, Type, Phone, Like
+//listen for a Like button to be pressed
 function renderFacility(facilityObj){
     const tableBody = document.getElementById("table-body")
     let tableRow = document.createElement("tr"); //is it ok to use innerHTML?
@@ -61,148 +69,26 @@ function renderFacility(facilityObj){
     tableBody.appendChild(tableRow) 
 
     let buttonLike = document.getElementById(`${facilityObj.FacilityID}`)
-    buttonLike.addEventListener('click', (event) => handleButtonLike(event, facilityObj))
+    buttonLike.addEventListener('click', (event) => handleButtonLike(event))
 }
-
-function handleButtonLike(event, facilityObj){
-    console.log(facilityObj.FacilityName)
+//function changes color of Like button (from corn to salmon or from salmon to corn color)
+function handleButtonLike(event){
     if(event.target.attributes[0].nodeValue === 'button-corn styled cursor'){
         event.target.attributes[0].nodeValue = 'button-salmon styled cursor'
     }else{
         event.target.attributes[0].nodeValue = 'button-corn styled cursor'
     }
 }
+//function uncheckes all checkboxes in form
 function handleClearAll(){
     const form = document.querySelector('#activity-form')
     form.reset() 
     clearTable()
 }
+//function select all checkboxes in form 
 function handleSelectAll(){
     const NodeListOfAllCheckBoxes = document.querySelectorAll('input[type = "checkbox"]')
     for(let i = 0; i < NodeListOfAllCheckBoxes.length; i++){
         NodeListOfAllCheckBoxes[i].checked = true
     }
 }
-// Facility type:
-
-
-// "Campground"
-// 3: "Ticket Facility"
-// 4: "Permit"
-// 5: "Facility"
-// 6: "Activity Pass"
-// 7: "Visitor Center"
-
-// 0: "Activity Pass"
-// 1: "Archives"
-// ? 2: "Campground"?
-// 3: "Cemetery and Memorial"
-// 4: "Construction Camp site"
-// 5: "Facility"
-// 6: "Federal"
-// 7: "Kiosk"
-// + 8: "Library"+
-// + 9: "Museum"+
-// + 10: "National Fish Hatchery"+
-// 11: "Permit"
-// 12: "State"
-// 13: "Ticket Facility"
-// +14: "Timed Entry" put like ticket+
-// +15: "Tree Permit"+
-// +16: "Visitor Center"+
-
-
-
-// function fetchAreas(){
-//     fetch("http://localhost:4000/RECDATA")
-//     .then(res => res.json())
-//     .then(areasArray => areasArray.forEach(areaObj => {
-//         //console.log(areaObj)
-//         renderArea(areaObj)}))
-// }
-
-// function fetchAreaAddress(){
-//     fetch("http://localhost:5000/RECDATA")
-//     .then(res => res.json())
-//     .then(areaAddresssArray => areaAddresssArray.forEach(areaAddressObj => {
-//        // console.log(areaAddressObj)
-
-//         renderAreaAddress(areaAddressObj)}))
-// }
-// function renderArea(areaObj){
-//     let area = document.createElement("li");
-//     area.innerHTML = `
-//     <h2>Area name: ${areaObj.RecAreaName}</h2>
-//     <p>Area directions: ${areaObj.RecAreaDirections}</p>
-//     `
-//     document.querySelector("body").appendChild(area)
-// }
-// let arr= ["State", "one more state"]
-// function renderAreaAddress(areaAddressObj){
-//     mark = 0;
-//     for(let i = 0; i < arr.length; i++){
-
-//         if(arr[i] === `${areaAddressObj.PostalCode}`){
-    
-//             mark = 1;
-//             break;
-//         } 
-//     }
-//     if(mark === 0){
-
-//         arr.push(`${areaAddressObj.PostalCode}`)
-//     }   
-//     console.log(arr.sort())
-//     let areaAddress = document.createElement("li");
-//     areaAddress.innerHTML = `
-//     <h2>Recreation area ID: ${areaAddressObj.RecAreaID}</h2>
-//     <h3>City: ${areaAddressObj.City}</h3>
-//     <p>State: ${areaAddressObj.PostalCode}</p>
-//     `
-//     document.querySelector("body").appendChild(areaAddress)
-// }
-
-//fetchAreaAddress();
-//fetchAreas();
-
-//<p>${areaObj.RecAreaDirections}</p> <p>${areaObj.RecAreaDescription}</p>
-//<h4>${areaObj.ParentOrgID}</h4> <h2>${areaObj.RecAreaName}</h2>
-//<p>${areaObj.RecAreaDescription}</p> <h4>${areaObj.FacilityTypeDescription}</h4>
-//<h4>${areaObj.Reservable}</h4> <h4>${areaObj.FacilityDescription}</h4>
-
-// function proccessData(facilitiesArray){
-//     const arrayOfCheckedInputs = readCheckBoxes()
-
-//     for(let i = 0; i < facilitiesArray.length; i++) {      //instead of forEach?
-//         sortFacility(facilitiesArray[i], arrayOfCheckedInputs);
-//     }
-// }
-
-// function sortFacility(facilityObj, arrayOfCheckedInputs) {
-//     for(let i = 0; i < arrayOfCheckedInputs.length; i++){
-//         if(facilityObj.FacilityTypeDescription === arrayOfCheckedInputs[i]){
-//             console.log(facilityObj)
-//             renderFacility(facilityObj)}
-//     }
-// }
-// function readCheckBoxes(){
-//     const arrayOfAllInputs = document.querySelectorAll('input[type = "checkbox"]:checked')
-//     console.log(arrayOfAllInputs)
-//     // let arrayOfCheckedInputs = []
-//     // for(let i = 0; i < arrayOfAllInputs.length - 3; i++) {  //how to get elemet Checkbox by type?
-//     //     if(arrayOfAllInputs[i].checked === true) {
-//     //         arrayOfCheckedInputs.push(arrayOfAllInputs[i].value)
-//     //     }
-//     // }
-//      return arrayOfCheckedInputs
-// }
-// function readCheckBoxes(){
-//     const arrayOfAllInputs = document.getElementsByTagName('input')
-//     let arrayOfCheckedInputs = []
-//     for(let i = 0; i < arrayOfAllInputs.length - 3; i++) {  //how to get elemet Checkbox by type?
-//         if(arrayOfAllInputs[i].checked === true) {
-//             arrayOfCheckedInputs.push(arrayOfAllInputs[i].value)
-//         }
-//     }
-//     return arrayOfCheckedInputs
-// }
